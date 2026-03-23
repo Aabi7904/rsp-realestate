668f1367258a42b19550f46681d2c867f87ceeb8
Admin <admin@Mac.lan>
2026-03-23 21:39:58 +0530

    changed since logo

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import { useLocation } from "react-router-dom";
 import heroImg from "@/assets/hero-land.jpg";
-import logoImg from "@/assets/since1.png";
+import logoImg from "@/assets/since.png";
 
 const HeroSection = () => {
   const location = useLocation();
